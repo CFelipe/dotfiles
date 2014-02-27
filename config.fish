1f9a47f739af59bc1fd3ac94106b963a8f7ec51d
Felipe Cortez <felipecortezfi@gmail.com>
2014-02-27 15:14:51 -0300

    Removed config.fish

--- config.fish
+++
@@ -1,4 +0,0 @@
-set fish_greeting ""
-alias vim "/Applications/MacVim.app/Contents/MacOS/Vim"
-alias love "/Applications/love.app/Contents/MacOS/love"
-alias octave "/Applications/Octave.app/Contents/Resources/bin/octave"
