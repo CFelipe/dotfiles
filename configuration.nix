ee9e8add7ce7a098aff05b7ed62f6b896a1a3d3c
Felipe Cortez <felipecortezfi@gmail.com>
2019-10-23 19:03:30 -0300

    Clean up configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -1,54 +1,39 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 
 {
   imports =
-    [ # Include the results of the hardware scan.
+    [
       ./hardware-configuration.nix
     ];
 
   environment.pathsToLink = [ "/libexec" ]; 
 
-  # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "bender"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant. sucks
-  networking.networkmanager.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.hostName = "bender";
+  networking.networkmanager.enable = true;
 
-  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
-  # Per-interface useDHCP will be mandatory in the future, so this generated config
-  # replicates the default behaviour.
   networking.useDHCP = false;
   networking.interfaces.enp2s0.useDHCP = true;
   networking.interfaces.wlp3s0.useDHCP = true;
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
   # Select internationalisation properties.
-  # i18n = {
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
   #   consoleFont = "Lat2-Terminus16";
   #   consoleKeyMap = "us";
-  #   defaultLocale = "en_US.UTF-8";
-  # };
+  };
 
   time.timeZone = "America/Sao_Paulo";
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
-    wget 
-    vim
-    firefox
     arandr
     deadbeef
     emacs
+    firefox
+    vim
+    wget 
     xclip
   ];
 
@@ -57,9 +42,6 @@
   # programs.mtr.enable = true;
   # programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
 
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
   services.openssh.enable = true;
 
   # Open ports in the firewall.
@@ -71,7 +53,6 @@
   # Enable CUPS to print documents.
   # services.printing.enable = true;
 
-  # Enable sound.
   sound.enable = true;
   hardware.pulseaudio.enable = true;
 
@@ -96,23 +77,11 @@
     };
   }; 
 
-
-  
-
-  # Enable the KDE Desktop Environment.
-  # services.xserver.displayManager.sddm.enable = true;
-  # services.xserver.desktopManager.plasma5.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
  users.users.felipe = {
    isNormalUser = true;
    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
  };
 
-  # This value determines the NixOS release with which your system is to be
-  # compatible, in order to avoid breaking some software such as database
-  # servers. You should change this only after NixOS release notes say you
-  # should.
-  system.stateVersion = "19.09"; # Did you read the comment?
+  system.stateVersion = "19.09";
 
 }
